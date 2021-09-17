04e1037f768928c56fcb2d7b63cb1586201d51da
Phung Xuan Truong <phungxuantruong@Phungs-MacBook-Pro.local>
2021-09-17 22:05:09 +0700

    init

---
+++ src/components/Content.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import {  tokenSelector,
+  accountSelector,
+  web3Selector,exchangeSelector } from '../store/selectors'
+import { loadAllOrders, subscribeToEvents } from '../store/interactions'
+import OrderBook from './OrderBook'
+import Trades from './Trades'
+import MyTransactions from './MyTransactions'
+import PriceChart from './PriceChart'
+import Balance from './Balance'
+import NewOrder from './NewOrder'
+
+class Content extends Component {
+  componentWillMount() {
+    this.loadBlockchainData(this.props)
+  }
+
+  async loadBlockchainData(props) {
+    const { dispatch, web3,token,exchange,account } = props
+    await loadAllOrders(exchange, dispatch)
+    await subscribeToEvents(web3,exchange,token,account, dispatch)
+  }
+
+  render() {
+    return (
+      <div className="content">
+        <div className="vertical-split">
+          <Balance />
+          <NewOrder />
+        </div>
+        <OrderBook />
+        <div className="vertical-split">
+          <PriceChart />
+          <MyTransactions />
+        </div>
+        <Trades />
+      </div>
+    )
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    account: accountSelector(state),
+    token: tokenSelector(state),
+    web3: web3Selector(state),
+    exchange: exchangeSelector(state)
+  }
+}
+
+export default connect(mapStateToProps)(Content)
